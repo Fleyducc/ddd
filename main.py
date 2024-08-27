7fb391015f2cd2b313c9e02a80f61172294dd778
Fleyducc <167763678+Fleyducc@users.noreply.github.com>
2024-08-27 15:04:18 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+ism = input("Hayvon:")
+
+og = input("Og'irligi:")
+
+rang = input("Rangi:")
+
+yosh = input("Yoshi:")
+
+ismiha = input("Ismi:")
+
+print(f"Hayvon: {ism} , Og'irligi: {og} , Rangi: {rang} , Yoshi: {yosh} , Ismi: {ismiha} ")
